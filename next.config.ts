e2d51f9ea96f8eaca7938c7b46d896b333bfb375
dkakasha500 <dkakasha500@users.noreply.github.com>
2026-04-17 23:54:06 +0300

    Switch to static HTML deployment

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* Оптимизация для продакшена */
-  poweredByHeader: false,
-  reactStrictMode: true,
-};
-
-export default nextConfig;
